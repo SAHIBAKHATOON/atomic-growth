d2419d846636d2ef253ec71baf963b3fc142ad46
SAHIBAKHATOON <sahibakhatoon2023@gmail.com>
2025-11-25 15:12:35 +0530

    sec commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useMemo, useState } from 'react'
 import './App.css'
-import Navbar from './components/navbar'
+import Navbar from './components/Navbar'
 import HeroSection from './components/HeroSection'
 import ServicesSection from './components/ServicesSection'
 import PortfolioSection from './components/PortfolioSection'
